a7289076c62021b46e3ed736592ad888262c7daa
Matt Windham <windham.matt@gmail.com>
2026-06-23 15:43:08 -0500

    Add GitHub Actions OIDC role and CI/CD pipeline for site infra
    terraform/github-oidc/ creates the OIDC trust between GitHub Actions
    and AWS (no stored keys) plus a least-privilege role scoped to only
    the resume-site S3 buckets and CloudFront management - no admin
    access, even though the trust policy allows any PR on this public
    repo to request a token (gated by GitHub's fork-PR approval).
    
    .github/workflows/terraform.yml runs fmt/validate/plan on PRs
    touching terraform/site/** and comments the plan, then auto-applies
    on merge to main. bootstrap/ and github-oidc/ stay out of CI -
    bootstrap uses local state by design, and the OIDC role's own
    permissions shouldn't be self-modifying via automation.

---
+++ terraform/github-oidc/main.tf
@@ -0,0 +1,125 @@
+data "tls_certificate" "github_actions" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
+}
+
+data "aws_iam_policy_document" "github_actions_trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        "repo:${var.github_repo}:ref:refs/heads/main",
+        "repo:${var.github_repo}:pull_request",
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions-resume-site"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_trust.json
+}
+
+data "aws_iam_policy_document" "github_actions_permissions" {
+  statement {
+    sid    = "TerraformStateAccess"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::resume-site-tfstate-955752000541",
+      "arn:aws:s3:::resume-site-tfstate-955752000541/*",
+    ]
+  }
+
+  statement {
+    sid    = "SiteBucketManagement"
+    effect = "Allow"
+    actions = [
+      "s3:CreateBucket",
+      "s3:DeleteBucket",
+      "s3:GetBucketPolicy",
+      "s3:PutBucketPolicy",
+      "s3:DeleteBucketPolicy",
+      "s3:GetBucketPublicAccessBlock",
+      "s3:PutBucketPublicAccessBlock",
+      "s3:GetBucketOwnershipControls",
+      "s3:PutBucketOwnershipControls",
+      "s3:GetBucketTagging",
+      "s3:PutBucketTagging",
+      "s3:GetBucketLocation",
+      "s3:GetBucketVersioning",
+      "s3:GetEncryptionConfiguration",
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+    resources = [
+      "arn:aws:s3:::resume-site-955752000541",
+      "arn:aws:s3:::resume-site-955752000541/*",
+    ]
+  }
+
+  statement {
+    sid    = "CloudFrontManagement"
+    effect = "Allow"
+    actions = [
+      "cloudfront:CreateDistribution",
+      "cloudfront:GetDistribution",
+      "cloudfront:UpdateDistribution",
+      "cloudfront:DeleteDistribution",
+      "cloudfront:TagResource",
+      "cloudfront:UntagResource",
+      "cloudfront:ListTagsForResource",
+      "cloudfront:CreateOriginAccessControl",
+      "cloudfront:GetOriginAccessControl",
+      "cloudfront:UpdateOriginAccessControl",
+      "cloudfront:DeleteOriginAccessControl",
+      "cloudfront:ListCachePolicies",
+      "cloudfront:GetCachePolicy",
+      "cloudfront:CreateInvalidation",
+      "cloudfront:GetInvalidation",
+      "cloudfront:ListInvalidations",
+      "cloudfront:ListDistributions",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid       = "STSIdentity"
+    effect    = "Allow"
+    actions   = ["sts:GetCallerIdentity"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions_permissions" {
+  name   = "resume-site-terraform-site"
+  role   = aws_iam_role.github_actions.id
+  policy = data.aws_iam_policy_document.github_actions_permissions.json
+}
