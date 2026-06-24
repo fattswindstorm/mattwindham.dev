7152d409c814161b227c076fe8ef113834637399
Matt Windham <fattswindstorm@users.noreply.github.com>
2026-06-24 11:26:32 -0500

    Add Open Graph/Twitter meta tags and a custom 404 page
    Link previews (LinkedIn, Slack, etc.) were rendering blank since
    there was no og:* or twitter:* metadata. Also wires up a styled
    404 page through CloudFront's custom_error_response, since the
    private S3 origin returns a raw AccessDenied 403 for any missing
    object today.

--- terraform/site/main.tf
+++ terraform/site/main.tf
@@ -118,6 +118,18 @@ resource "aws_cloudfront_distribution" "site" {
     }
   }
 
+  custom_error_response {
+    error_code         = 403
+    response_code      = 404
+    response_page_path = "/404.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 404
+    response_page_path = "/404.html"
+  }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
